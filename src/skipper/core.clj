02ba9e926530a046525af8ff5967752408061786
Loukas Agorgianitis <loukas@agorgianitis.com>
2024-09-01 10:31:09 +0300

    project: cleanup generated sources

--- src/skipper/core.clj
+++ src/skipper/core.clj
@@ -1,12 +1,5 @@
 (ns skipper.core
   (:gen-class))
 
-(defn greet
-  "Callable entry point to the application."
-  [data]
-  (println (str "Hello, " (or (:name data) "World") "!")))
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (greet {:name (first args)}))
+(defn -main [& args]
+  (println "Hello there"))
